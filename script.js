685f86b0929c9977d9c5bed28c67583af2b9eab7
Sarah Linkous <sralinkous@gmail.com>
2017-04-06 16:42:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+  $('#main').append("text will go here");
+
+  $('#navbar li').on('click', function(){
+    $('#navbar li').removeClass();
+    $(this).addClass("active");
+  });
+
+  $('#navbar li a').on('click', function(){
+    $('.contents').hide();
+    var section = $(this).attr('href');
+    $(section).show();
+  })
+
+});
